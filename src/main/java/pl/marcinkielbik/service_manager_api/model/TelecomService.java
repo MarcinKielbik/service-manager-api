38405014f4acd644877dfbc79d571ca6bdd0888e
Marcin Kielbik <marcinlukasz.kielbik@gmail.com>
2025-06-10 10:38:17 +0200

    Create TelecomService model

--- src/main/java/pl/marcinkielbik/service_manager_api/model/TelecomService.java
+++ src/main/java/pl/marcinkielbik/service_manager_api/model/TelecomService.java
@@ -1,5 +1,23 @@
 package pl.marcinkielbik.service_manager_api.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "telecom_services")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TelecomService {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+    private double price;
 }
